46b2bdabbfb66c79fa4c0baf886c39aed1d50c56
ClaudeCalewaert <claudecalewaert86@hotmail.com>
2018-02-19 21:51:44 -0600

    Se implemento el codigo para los metodos para la Clase Stack basada en ArrayList (StackArrayList)

--- HDT4-Calculadora/src/StackArrayList.java
+++ HDT4-Calculadora/src/StackArrayList.java
@@ -1,28 +1,45 @@
 import java.util.ArrayList;
 
+/**
+ * @author Claude Calewaert
+ * @param <T>
+ * @Proposito Clase que modela un Stack basandose en un ArrayList
+ *
+ */
+
 public class StackArrayList<T> extends Stack<T>{
 
 	// Declaracion de variables
 	
 	private ArrayList<T> arreglo;
 
+	// Constructor
+	
+	public StackArrayList() {
+		arreglo = new ArrayList<>();
+	}
 	
 	// Metodos
 	
-	public void push(T dato) {
-		
+	public void push(T dato) { // Ingresa un elemento
+		arreglo.add(dato);
 	}
 
 
-	public T pop() {
+	public T pop() { // Extrae un elemento
 	
-		return null;
+		T ultimo;
+		
+		ultimo = arreglo.get(arreglo.size()-1);
+		arreglo.remove(ultimo);
+		
+		return ultimo;
 	}
 
 
-	public Boolean EstaLleno() {
-		
-		return null;
+	public Boolean EstaVacio() { // Verifica si el Stack esta vacio
+		return arreglo.isEmpty();
+	
 	}
 
 	
