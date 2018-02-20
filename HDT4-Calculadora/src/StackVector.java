1df5aee7234946098a48cdc29084cc8175b883e9
ClaudeCalewaert <claudecalewaert86@hotmail.com>
2018-02-19 22:27:25 -0600

    Se implemento el codigo para los metodos del Stack basado en vectores, clase StackVector

--- HDT4-Calculadora/src/StackVector.java
+++ HDT4-Calculadora/src/StackVector.java
@@ -1,4 +1,35 @@
+import java.util.Vector;
 
-public class StackVector extends Stack {
+public class StackVector<T> extends Stack<T> {
+
+	// Declaracion de variables
+	
+	private Vector<T> vec;
+	
+	// Constructor
+	
+	public StackVector() {
+		vec = new Vector<T>();
+	}
+	
+	// Metodos
+	
+	public void push(T dato) {	// Ingresa un dato al Stack
+		vec.addElement(dato);
+	}
+
+	
+	public T pop() { // Extrae un dato del Stack
+		
+		T ultimo = vec.get(vec.size()-1);
+		vec.removeElementAt(vec.size()-1);;
+		
+		return ultimo;
+	}
+
+	
+	public Boolean EstaVacio() { // Devueleve si el Stack esta vacio
+		return vec.isEmpty();
+	}
 
 }
